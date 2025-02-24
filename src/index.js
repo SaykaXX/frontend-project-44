93f1ffce47a3eba8050e37c78d6abcd43a3a8ce5
Miram <vtstayvtatstm@gmail.com>
2025-02-24 17:19:58 +0700

    Add files via upload
    miram3

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greetingMessage = () => {
+    const name = readlineSync.question('May I have your name?')
+    console.log(`Hi, ${name}`);
+};
+
+export {greetingMessage}
