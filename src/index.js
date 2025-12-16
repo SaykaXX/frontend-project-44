9bc45c78d6ea65e7e355ff1cbad04e5ff4e3bc61
Miram <vtstayvtatstm@gmail.com>
2025-12-16 10:05:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,26 @@ const greetingsMsg = 'Welcome to the Brain Games!'
 const timeCount = 3
 
 const startEngine = (rule, getGameOptions) => {
-  console.log(${greetingsMsg})
+  console.log(`${greetingsMsg}`)
   const userName = readlineSync.question('May I have your name? ')
-  console.log(Hello, ${userName}!)
-  console.log(${rule})
+  console.log(`Hello, ${userName}!`)
+  console.log(`${rule}`)
   for (let i = 0; i < timeCount; i += 1) {
     const { expression: questionExpression, answer: correctAnswer } = getGameOptions()
-    console.log(Question: ${questionExpression})
+    console.log(`Question: ${questionExpression}`)
     const userAnswer = readlineSync.question('Your answer: ')
     if (correctAnswer === userAnswer) {
       console.log('Correct!')
     }
     else {
       console.log('${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.)
-      console.log(Let's try again, ${userName}!)
+      console.log(`Let's try again, ${userName}!`)
       return
     }
   }
 
-  console.log(Congratulations, ${userName}!)
+  console.log(`Congratulations, ${userName}!`)
 }
 
 export default startEngine
+
